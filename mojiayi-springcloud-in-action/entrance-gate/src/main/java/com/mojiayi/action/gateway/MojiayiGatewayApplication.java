71420ea1a4d382c45f1232036aea5fd2a10c6c6a
Mojiayi <woshimojiayi@gmail.com>
2021-12-23 22:45:37 +0800

    update:使用gateway做请求路由

---
+++ mojiayi-springcloud-in-action/entrance-gate/src/main/java/com/mojiayi/action/gateway/MojiayiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.mojiayi.action.gateway;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @author liguangri
+ */
+@SpringBootApplication
+@Slf4j
+public class MojiayiGatewayApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(MojiayiGatewayApplication.class, args);
+        log.info("Startup MojiayiGatewayApplication");
+    }
+}
