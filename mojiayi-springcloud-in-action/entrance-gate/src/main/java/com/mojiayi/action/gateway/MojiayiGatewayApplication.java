1feaeb24c33de4cb210e97e3c4a242d917d6a31e
Mojiayi <woshimojiayi@gmail.com>
2021-12-23 23:01:27 +0800

    update:支持gateway+nacos服务负载做请求路由

--- mojiayi-springcloud-in-action/entrance-gate/src/main/java/com/mojiayi/action/gateway/MojiayiGatewayApplication.java
+++ mojiayi-springcloud-in-action/entrance-gate/src/main/java/com/mojiayi/action/gateway/MojiayiGatewayApplication.java
@@ -3,10 +3,12 @@ package com.mojiayi.action.gateway;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 /**
  * @author liguangri
  */
+@EnableDiscoveryClient
 @SpringBootApplication
 @Slf4j
 public class MojiayiGatewayApplication {
