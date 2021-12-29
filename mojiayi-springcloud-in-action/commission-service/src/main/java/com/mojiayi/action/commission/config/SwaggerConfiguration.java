e800798b7b3fd2c7d4a321faf18831e0d2c83674
Mojiayi <woshimojiayi@gmail.com>
2021-12-29 18:27:50 +0800

    update:gateway集成knife4j

---
+++ mojiayi-springcloud-in-action/commission-service/src/main/java/com/mojiayi/action/commission/config/SwaggerConfiguration.java
@@ -0,0 +1,46 @@
+package com.mojiayi.action.commission.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Import;
+import org.springframework.core.annotation.Order;
+import springfox.bean.validators.configuration.BeanValidatorPluginsConfiguration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
+import springfox.documentation.service.Contact;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+
+/**
+ * @author liguangri
+ */
+@Configuration
+@EnableSwagger2
+@Import(BeanValidatorPluginsConfiguration.class)
+public class SwaggerConfiguration {
+
+    @Bean(value = "commissionServiceApi")
+    @Order(value = 1)
+    public Docket groupRestApi() {
+        return new Docket(DocumentationType.SWAGGER_2)
+                .apiInfo(groupApiInfo())
+                .select()
+                .apis(RequestHandlerSelectors.basePackage("com.mojiayi.action.commission.controller"))
+                .paths(PathSelectors.any())
+
+                .build();
+    }
+
+    private ApiInfo groupApiInfo() {
+        return new ApiInfoBuilder()
+                .title("swagger-bootstrap-ui很棒~~~！！！")
+                .description("<div style='font-size:14px;color:red;'>swagger-bootstrap-ui-demo RESTful APIs</div>")
+                .termsOfServiceUrl("http://www.mojiayi.com/")
+                .contact(new Contact("mojiayi", "http://www.mojiayi.com", "woshimojiayi@163.com"))
+                .version("1.0")
+                .build();
+    }
+}
