4fc08075a1809da0ded4aa3a7df797a8841393ab
Mojiayi <woshimojiayi@gmail.com>
2021-12-31 14:55:12 +0800

    update:保留3种冒泡排序算法的实现方式

---
+++ mojiayi-algorithm-in-action/src/test/java/com/mojiayi/action/algorithm/SortingTest.java
@@ -0,0 +1,45 @@
+package com.mojiayi.action.algorithm;
+
+import com.mojiayi.action.algorithm.sort.impl.ExclusiveOrBubbleSorting;
+import com.mojiayi.action.algorithm.sort.impl.NoTempDataSwapBubbleSorting;
+import com.mojiayi.action.algorithm.sort.impl.TraditionalSwapBubbleSorting;
+import org.junit.Assert;
+import org.junit.Test;
+
+public class SortingTest {
+    private static final int[] inputData = {7, 4, 8, 2, 3, 5, 6, 1};
+
+    @Test
+    public void testTraditionalBubbleSorting() {
+        TraditionalSwapBubbleSorting sortingAlgorithm = new TraditionalSwapBubbleSorting();
+        int[] sortedData = sortingAlgorithm.sort(inputData);
+        verifySortResult(sortedData);
+    }
+
+    @Test
+    public void testNoTempDataBubbleSorting() {
+        NoTempDataSwapBubbleSorting sortingAlgorithm = new NoTempDataSwapBubbleSorting();
+        int[] sortedData = sortingAlgorithm.sort(inputData);
+        verifySortResult(sortedData);
+    }
+
+    @Test
+    public void testExclusiveOrBubbleSorting() {
+        ExclusiveOrBubbleSorting sortingAlgorithm = new ExclusiveOrBubbleSorting();
+        int[] sortedData = sortingAlgorithm.sort(inputData);
+        verifySortResult(sortedData);
+    }
+
+    private void verifySortResult(int[] sortedData) {
+        int expected = 1;
+        Assert.assertEquals(expected, sortedData[expected - 1]);
+        expected = 3;
+        Assert.assertEquals(expected, sortedData[expected - 1]);
+        expected = 5;
+        Assert.assertEquals(expected, sortedData[expected - 1]);
+        expected = 7;
+        Assert.assertEquals(expected, sortedData[expected - 1]);
+        expected = 6;
+        Assert.assertEquals(expected, sortedData[expected - 1]);
+    }
+}
