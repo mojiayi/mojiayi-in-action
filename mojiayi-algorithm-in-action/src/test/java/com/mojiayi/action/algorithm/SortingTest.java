51809852dfc05b8e1a1cbb6a0a8587e10cddabe8
Mojiayi <woshimojiayi@gmail.com>
2022-01-04 10:25:25 +0800

    update:保存不新建一个数组的选择排序算法实现方案

--- mojiayi-algorithm-in-action/src/test/java/com/mojiayi/action/algorithm/SortingTest.java
+++ mojiayi-algorithm-in-action/src/test/java/com/mojiayi/action/algorithm/SortingTest.java
@@ -4,6 +4,7 @@ import com.mojiayi.action.algorithm.sort.impl.BubbleSortingExclusiveOr;
 import com.mojiayi.action.algorithm.sort.impl.BubbleSortingNoTempDataSwap;
 import com.mojiayi.action.algorithm.sort.impl.InsertSortingRememberInsertPoint;
 import com.mojiayi.action.algorithm.sort.impl.InsertSortingTraditional;
+import com.mojiayi.action.algorithm.sort.impl.SelectionSortingOnlyOneArray;
 import com.mojiayi.action.algorithm.sort.impl.SelectionSortingTraditional;
 import com.mojiayi.action.algorithm.sort.impl.BubbleSortingTraditionalSwap;
 import org.junit.Assert;
@@ -13,47 +14,53 @@ public class SortingTest {
     private static final int[] inputData = {7, 4, 8, 2, 3, 5, 6, 1};
 
     @Test
-    public void testTraditionalBubbleSorting() {
+    public void testBubbleSortingTraditionalSwap() {
         BubbleSortingTraditionalSwap sortingAlgorithm = new BubbleSortingTraditionalSwap();
         int[] sortedData = sortingAlgorithm.sort(inputData);
         verifySortResult(sortedData);
     }
 
     @Test
-    public void testNoTempDataBubbleSorting() {
+    public void testBubbleSortingNoTempDataSwap() {
         BubbleSortingNoTempDataSwap sortingAlgorithm = new BubbleSortingNoTempDataSwap();
         int[] sortedData = sortingAlgorithm.sort(inputData);
         verifySortResult(sortedData);
     }
 
     @Test
-    public void testExclusiveOrBubbleSorting() {
+    public void testBubbleSortingExclusiveOr() {
         BubbleSortingExclusiveOr sortingAlgorithm = new BubbleSortingExclusiveOr();
         int[] sortedData = sortingAlgorithm.sort(inputData);
         verifySortResult(sortedData);
     }
 
     @Test
-    public void testTraditionalInsertSorting() {
+    public void testInsertSortingTraditional() {
         InsertSortingTraditional sortingAlgorithm = new InsertSortingTraditional();
         int[] sortedData = sortingAlgorithm.sort(inputData);
         verifySortResult(sortedData);
     }
 
     @Test
-    public void testRememberInsertPointInsertSorting() {
+    public void testInsertSortingRememberInsertPoint() {
         InsertSortingRememberInsertPoint sortingAlgorithm = new InsertSortingRememberInsertPoint();
         int[] sortedData = sortingAlgorithm.sort(inputData);
         verifySortResult(sortedData);
     }
 
     @Test
-    public void testTraditionalSelectionSorting() {
+    public void testSelectionSortingTraditional() {
         SelectionSortingTraditional sortingAlgorithm = new SelectionSortingTraditional();
         int[] sortedData = sortingAlgorithm.sort(inputData);
         verifySortResult(sortedData);
     }
 
+    @Test
+    public void testSelectionSortingOnlyOneArray() {
+        SelectionSortingOnlyOneArray sortingAlgorithm = new SelectionSortingOnlyOneArray();
+        int[] sortedData = sortingAlgorithm.sort(inputData);
+        verifySortResult(sortedData);
+    }
     private void verifySortResult(int[] sortedData) {
         int expected = 1;
         Assert.assertEquals(expected, sortedData[expected - 1]);
