232fd2be8725e608abb071e651aab4b7533af6f3
liguangri <liguangri@wdtrgf.com.cn>
2022-02-10 18:03:42 +0800

    feat:修改责任链模式的下单请求校验流程测试代码文件名

--- mojiayi-design-pattern-in-action/src/test/java/com/mojiayi/action/designpattern/responsibilitychain/VerifyPurchaseRequestTest.java
+++ mojiayi-design-pattern-in-action/src/test/java/com/mojiayi/action/designpattern/responsibilitychain/VerifyPurchaseRequestTest.java
@@ -12,7 +12,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringBootTest(classes = Application.class)
-public class ResponsibilityChainTest {
+public class VerifyPurchaseRequestTest {
     @Autowired
     private ChainBuilder chainBuilder;
 
