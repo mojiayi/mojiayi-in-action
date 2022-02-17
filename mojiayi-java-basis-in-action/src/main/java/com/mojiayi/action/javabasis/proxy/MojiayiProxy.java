fcd12d85e2d24d99a1690e3ffce79b8beb759bdf
Mojiayi <woshimojiayi@gmail.com>
2022-02-17 16:04:22 +0800

    update:保存通过代理方式计算方法调用时长的代码

---
+++ mojiayi-java-basis-in-action/src/main/java/com/mojiayi/action/javabasis/proxy/MojiayiProxy.java
@@ -0,0 +1,39 @@
+package com.mojiayi.action.javabasis.proxy;
+
+import java.lang.reflect.InvocationHandler;
+import java.lang.reflect.Method;
+import java.lang.reflect.Proxy;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+/**
+ * @author liguangri
+ */
+public class MojiayiProxy {
+    public static void main(String[] args) {
+        myInstance();
+    }
+
+    private static void myInstance() {
+        Collection proxy = (Collection) Proxy.newProxyInstance(Collection.class.getClassLoader(), new Class[]{Collection.class}, new InvocationHandler() {
+            final List<?> target = new ArrayList<>();
+            @Override
+            public Object invoke(Object proxy, Method method, Object[] args) throws Throwable {
+                long startTime = System.currentTimeMillis();
+                Thread.sleep(1000L);
+                Object value = method.invoke(target, args);
+                long finishTime = System.currentTimeMillis();
+                System.out.println("方法" + method.getName() + "耗时=" + (finishTime - startTime) + "ms");
+                return value;
+            }
+        });
+
+        proxy.add("a");
+        proxy.add("b");
+        proxy.add("c");
+        proxy.add("d");
+        proxy.remove("b");
+        proxy.size();
+    }
+}
